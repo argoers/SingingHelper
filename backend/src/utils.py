5f54e381703062af1e8ec56e2f2aac2067251f47
Argo Ers <52284347+argoers@users.noreply.github.com>
2025-04-28 15:11:18 +0300

    fix: windows app quitting

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,12 +1,21 @@
 import time
 import os
+import platform
 
 def shutdown_backend():
     # Wait for 1 second before shutting down
     time.sleep(1)
 
-    # Kill any process using port 5173 (Development frontend Vite server)
-    os.system("lsof -t -i:5173 | xargs kill")
+    # Check the operating system and execute the appropriate command to kill processes
+    if platform.system() == "Windows":
+        # Kill any process using port 5173 (Development frontend Vue server)
+        os.system('for /f "tokens=5" %a in (\'netstat -aon ^| findstr LISTENING ^| findstr :5173\') do taskkill /PID %a /F')
 
-    # Kill any process using port 5001 (Final app or Flask development backend)
-    os.system("lsof -t -i:5001 | xargs kill")
+        # Kill any process using port 5001 (Final app or Flask development backend)
+        os.system('for /f "tokens=5" %a in (\'netstat -aon ^| findstr LISTENING ^| findstr :5001\') do taskkill /PID %a /F')
+    else:
+        # Kill any process using port 5173 (Development frontend Vue server)
+        os.system("lsof -t -i:5173 | xargs kill")
+
+        # Kill any process using port 5001 (Final app or Flask development backend)
+        os.system("lsof -t -i:5001 | xargs kill")
